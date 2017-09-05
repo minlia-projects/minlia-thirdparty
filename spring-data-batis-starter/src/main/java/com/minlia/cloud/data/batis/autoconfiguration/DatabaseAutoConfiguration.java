0929a0cc2eb173c488b6f6d0c3484df73711c8c8
will <will@minlia.com>
2017-09-06 00:25:01 +0800

    Update at 20170906002458 by dev/will

--- spring-data-batis-starter/src/main/java/com/minlia/cloud/data/batis/autoconfiguration/DatabaseAutoConfiguration.java
+++ spring-data-batis-starter/src/main/java/com/minlia/cloud/data/batis/autoconfiguration/DatabaseAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.minlia.cloud.data.batis.autoconfiguration;
 
+import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,19 +9,14 @@ import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mybatis.domains.AuditDateAware;
 import org.springframework.data.mybatis.replication.transaction.ReadWriteManagedTransactionFactory;
 import org.springframework.data.mybatis.repository.config.EnableMybatisRepositories;
-import org.springframework.data.mybatis.support.GuidHolder;
 import org.springframework.data.mybatis.support.SqlSessionFactoryBean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.Date;
-import org.threeten.bp.ZonedDateTime;
 
 
 @Configuration
@@ -51,36 +47,28 @@ public class DatabaseAutoConfiguration implements ResourceLoaderAware {
         return new DataSourceTransactionManager(dataSource);
     }
 
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new AuditorAware<String>() {
-            @Override
-            public String getCurrentAuditor() {
-                return GuidHolder.getGuid();
-            }
-        };
-    }
+
+    //defined in rebecca project
+//    @Bean
+//    public AuditorAware<String> auditorAware() {
+//        return new AuditorAware<String>() {
+//            @Override
+//            public String getCurrentAuditor() {
+//                return DefaultGuidHolder.getGuid();
+//            }
+//        };
+//    }
 
     @Bean
     @ConditionalOnMissingBean
-    public AuditDateAware<ZonedDateTime> auditDateAware() {
-        return new AuditDateAware<ZonedDateTime>() {
+    public AuditDateAware<LocalDateTime> auditDateAware() {
+        return new AuditDateAware<LocalDateTime>() {
             @Override
-            public ZonedDateTime getCurrentDate() {
-                return ZonedDateTime.now();
+            public LocalDateTime getCurrentDate() {
+                return LocalDateTime.now();
             }
         };
     }
-
-//    @Bean
-//    AuditorAware<String> auditorProvider() {
-//        return new UsernameAuditorAware();
-//    }
-//
-//    @Bean
-//    DateTimeProvider dateTimeProvider(DateTimeService dateTimeService) {
-//        return new AuditingDateTimeProvider(dateTimeService);
-//    }
 
 
     @Override
--- spring-data-batis/src/main/java/org/springframework/data/mybatis/support/GuidHolder.java
+++ spring-data-batis/src/main/java/org/springframework/data/mybatis/support/GuidHolder.java
@@ -4,8 +4,7 @@ package org.springframework.data.mybatis.support;
  * Created by will on 8/17/17.
  * 默认系统用户以10000开始, 后面递增
  */
-public class GuidHolder {
-    public static String getGuid(){
-        return "10000";
-    }
+public interface GuidHolder {
+    public static final String DEFAULT_GUID_HOLDER_VALUE="10000";
+    public String getGuid();
 }
